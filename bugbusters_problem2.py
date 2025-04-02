7e17a6f50825f9640c7fb76ee665aaaf6b95f15a
Annahajaveedmir <annahajaveedmir@gmail.com>
2025-04-02 01:04:22 -0700

    Add files via upload

---
+++ bugbusters_problem2.py
@@ -0,0 +1,20 @@
+#annaha
+n1=input("enter the first roman numeral")
+n2=input("enter the second roman numeral ")
+integer=int(n1,n2)
+numeral=['I'=1,'V'=5,'L'=50,'D'=500,'M'=1000,'X'=10,'C'=100]
+sum=0
+if integer<1000:
+    sum=n1+n2
+    sum=numeral.list
+else:
+    print("not valid enter a number lesser than 1000")
+finalnumeral=str(sum)
+for sum<10:
+    char in sum
+
+print("final sum is:",sum)
+
+
+
+
