3a135974cebd9a0c7f6b1f88c4b7a13c6c5021ef
Annahajaveedmir <annahajaveedmir@gmail.com>
2025-04-02 01:52:12 -0700

    Update bugbusters_problem2.py

--- bugbusters_problem2.py
+++ bugbusters_problem2.py
@@ -2,18 +2,35 @@
 n1=input("enter the first roman numeral")
 n2=input("enter the second roman numeral ")
 integer=int(n1,n2)
-numeral=['I'=1,'V'=5,'L'=50,'D'=500,'M'=1000,'X'=10,'C'=100]
+numeral={'I':1,'V':5,'L':50,'D':500,'M':1000,'X':10,'C':100}
 sum=0
+finalr=0
 if integer<1000:
     sum=n1+n2
-    sum=numeral.list
+    sum=numeral.dict
 else:
     print("not valid enter a number lesser than 1000")
 finalnumeral=str(sum)
-for sum<10:
-    char in sum
+for char in sum:
+    if char==M:
+        finalr=finalr+1000
+    elif char==D:
+        finalr=finalr+500
+    elif char==C:
+        finalr=finalr+100
+    elif char==L:
+        finalr=finalr+50
+    elif char==X:
+        finalr=finalr+10
+    elif char==V:
+        finalr==finalr+5
+    else:
+        finalr=finalr+1
+            
 
 print("final sum is:",sum)
+print("the sum of the integers to roman= ",finalr)
+
 
 
 
